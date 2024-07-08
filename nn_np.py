b2fc6ee1ba9fdbc79bdc8bf824f05f0857b7da08
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2024-07-07 18:39:18 -0700

    connecting multiple layers and np translation

--- nn_np.py
+++ nn_np.py
@@ -1,13 +1,23 @@
 # Some equivalent implementations using numpy library
 import numpy as np
 
-weights = np.array([0.1, 0.2, 0])
+ih_weights = np.array([[0.1, 0.2, -0.1],
+                       [-0.1, 0.1, 0.9], 
+                       [0.1, 0.4, 0.1] ]).T
 
-def neural_network(inputs, weights):
-    res = inputs@weights
-    return res
+hp_weights = np.array([[0.3, 1.1, -0.3],
+                       [0.1, 0.2, 0.0], 
+                       [0.0, 1.3, 0.1] ]).T
 
-weights = np.array([0.1, 0.2, 0])
+weights = [ih_weights, hp_weights]
+
+
+def neural_network(input, weights):
+    hidden = input @ weights[0]
+    pred = hidden @ weights[1]
+    return pred
+
+
 toes = np.array([8.5, 9.5, 9.9, 9.0])
 wlrec = np.array([0.65, 0.8, 0.8, 0.9])
 nfans = np.array([1.2, 1.3, 0.5, 1.0])
